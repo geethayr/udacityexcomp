fb6d2ba9ca8872b923f6c041c7eed325bb9bf67f
Geetha Yodham <geethayodham@gmail.com>
2020-05-07 12:10:18 -0500

    changing class to func

--- src/App.js
+++ src/App.js
@@ -1,141 +1,113 @@
-import React, { Component } from "react";
-import logo from "../logo.svg";
-import "../App.css";
-import Dashboard from "./Dashboard";
+import React from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+import MovieCardsList from './MovieCardsList';
 
 const profiles = [
     {
         id: 1,
-        userID: "1",
-        favoriteMovieID: "1"
+        userID: '1',
+        favoriteMovieID: '1',
     },
     {
         id: 2,
-        userID: "2",
-        favoriteMovieID: "1"
+        userID: '2',
+        favoriteMovieID: '1',
     },
     {
         id: 3,
-        userID: "4",
-        favoriteMovieID: "5"
+        userID: '4',
+        favoriteMovieID: '5',
     },
     {
         id: 4,
-        userID: "5",
-        favoriteMovieID: "2"
+        userID: '5',
+        favoriteMovieID: '2',
     },
     {
         id: 5,
-        userID: "3",
-        favoriteMovieID: "5"
+        userID: '3',
+        favoriteMovieID: '5',
     },
     {
         id: 6,
-        userID: "6",
-        favoriteMovieID: "4"
-    }
+        userID: '6',
+        favoriteMovieID: '4',
+    },
 ];
 
 const users = {
     1: {
         id: 1,
-        name: "Jane Jones",
-        userName: "coder"
+        name: 'Jane Jones',
+        userName: 'coder',
     },
     2: {
         id: 2,
-        name: "Matthew Page",
-        userName: "mpage"
+        name: 'Matthew Page',
+        userName: 'mpage',
     },
     3: {
         id: 3,
-        name: "Autumn Green",
-        userName: "user123"
+        name: 'Autumn Green',
+        userName: 'user123',
     },
     4: {
         id: 3,
-        name: "John Doe",
-        userName: "user123"
+        name: 'John Doe',
+        userName: 'user123',
     },
     5: {
         id: 5,
-        name: "Lauren Johnson",
-        userName: "user123"
+        name: 'Lauren Johnson',
+        userName: 'user123',
     },
     6: {
         id: 6,
-        name: "Nicholas Lain",
-        userName: "user123"
-    }
+        name: 'Nicholas Lain',
+        userName: 'user123',
+    },
 };
 
 const movies = {
     1: {
         id: 1,
-        name: "Planet Earth 1"
+        name: 'Planet Earth 1',
     },
     2: {
         id: 2,
-        name: "Selma"
+        name: 'Selma',
     },
     3: {
         id: 3,
-        name: "Million Dollar Baby"
+        name: 'Million Dollar Baby',
     },
     4: {
         id: 4,
-        name: "Forrest Gump"
+        name: 'Forrest Gump',
     },
     5: {
         id: 5,
-        name: "Get Out"
-    }
+        name: 'Get Out',
+    },
 };
 
-class App extends Component {
-    /*
-    The constructor is a "special method for creating and initializing an object."
-    (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes). The
-    Component's constructor is the first thing that runs when the object is created.
-    */
-    constructor(props) {
-        super(props);
-        this.usersByMovie = {};
+/*
+Since this component doesn't need to hold state, we can make it a Stateless
+Functional Component.
+*/
 
-        /*
-        We can map the users by the movie they liked.
-        */
-        profiles.forEach(profile => {
-            const movieID = profile.favoriteMovieID;
-
-            if (this.usersByMovie[movieID]) {
-                this.usersByMovie[movieID].push(profile.userID);
-            } else {
-                this.usersByMovie[movieID] = [profile.userID];
-            }
-        });
-    }
-
-    /*
-    The render method gets called automatically every time the value of the
-    component's props changes.
-    */
-    render() {
-        return (
-            <div>
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">ReactND - Coding Practice</h1>
-                </header>
-                <h2>How Popular is Your Favorite Movie?</h2>
-                <Dashboard
-                    usersByMovie={this.usersByMovie}
-                    movies={movies}
-                    users={users}
-                />
-            </div>
-        );
-    }
-}
+const App = () => {
+    return (
+        <div>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">ReactND - Coding Practice</h1>
+            </header>
+            <h1>How Popular is Your Favorite Movie?</h1>
+            <MovieCardsList profiles={profiles} movies={movies} users={users} />
+        </div>
+    );
+};
 
 export default App;
